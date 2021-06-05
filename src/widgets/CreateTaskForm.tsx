6928d2df2ef947efa8ae7fb057e59e721394f19d
christianquispe <christianquispecamasca@gmail.com>
2021-06-05 04:58:38 -0500

    chore: add custom tailwind styles for colors

--- src/widgets/CreateTaskForm.tsx
+++ src/widgets/CreateTaskForm.tsx
@@ -19,12 +19,14 @@ const CreateTaskForm = () => {
   })
 
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <input name="name" onChange={formik.handleChange} value={formik.values.name} type="text" />
-      <input name="priority" onChange={formik.handleChange} value={formik.values.priority} type="text" />
-      <input name="description" onChange={formik.handleChange} value={formik.values.description} type="text" />
-      <input type="submit" value="Crear" />
-    </form>
+    <div className="bg-gray-600 rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg md:max-w-2xl">
+      <form onSubmit={formik.handleSubmit}>
+        <input name="name" onChange={formik.handleChange} value={formik.values.name} type="text" className="border border-gray-800 focus:border-blue-500 rounded w-full py-2 px-3 mr-4 text-black mb-4" />
+        <input name="priority" onChange={formik.handleChange} value={formik.values.priority} type="text" className="border border-gray-800 focus:border-blue-500 rounded w-full py-2 px-3 mr-4 text-black mb-4" />
+        <input name="description" onChange={formik.handleChange} value={formik.values.description} type="text" className="border border-gray-800 focus:border-blue-500 rounded w-full py-2 px-3 mr-4 text-black mb-4" />
+        <input type="submit" value="Crear" />
+      </form>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Build your palette here
+        transparent: 'transparent',
+        current: 'currentColor',
+        gray: colors.blueGray,
+        red: colors.red,
+        blue: colors.lightBlue,
+        yellow: colors.amber,
+      }
+    },
   },
   variants: {
     extend: {},
